5997dca73856cbb67022f6da857aebb77ca081c7
Robert C. Sheets <rcsheets@acm.org>
2013-02-11 22:44:29 -0800

    Changes to db/schema.rb resulting from removal of "trackable"

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130209022750) do
+ActiveRecord::Schema.define(:version => 20130212043115) do
 
   create_table "events", :force => true do |t|
     t.string   "title"
@@ -39,11 +39,6 @@ ActiveRecord::Schema.define(:version => 20130209022750) do
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          :default => 0
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
   end
