d224d90bea177c0a514c7af5a1e247cea6a7bbbd
Robert C. Sheets <rcsheets@acm.org>
2013-02-12 05:33:20 -0800

    added request logging to ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :log_request
+
+  protected
+    def log_request
+      req = WebRequest.new
+      req.guid = request.uuid
+      req.save
+    end
 end
